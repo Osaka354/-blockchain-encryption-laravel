88b761b8875de739a797266c37da3db857aad4e6
ossasoeun <ossasoeun9@gmail.com>
2022-07-05 11:29:53 +0700

    First Desploy

---
+++ app/Http/Controllers/TextController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+USE App\Models\Text;
+
+class TextController extends Controller
+{
+    public function index() {
+        $data = Text::all();
+
+        $json = json_encode($data);
+        $encrypted = $this->encrypt($json);
+
+        return response(["data" => $encrypted], 200);
+    }
+
+    public function store(Request $request) {
+        $request->validator([
+            "content" => "required",
+        ]);
+
+        $content = $request->content;
+        $decrypted = $this->decrypt($content);
+
+        $text = new Text();
+        $text->content = $decrypted;
+        $text->save();
+
+        return response(["data" => $content], 200);
+    }
+
+    public function get_encrypyt(Request $request) 
+    {
+        $request->validator([
+            "content" => "required",
+        ]);
+        
+        $request->validator([
+            "content" => "required",
+        ]);
+        return $this->encrypt($request->content);
+    }
+
+    public function get_decrypyt(Request $request) {
+        return $this->decrypt($request->content);
+    }
+
+    private function get_key() {
+        $key = pack("H*", "21312345FFDCE3432521FDCD");
+        return $key;
+    }
+
+    private function encrypt($plain_text)
+    {
+        $cipher = openssl_encrypt($plain_text, "DES-EDE3", $this->get_key());
+        $cipher_bash64 = base64_encode($cipher);
+        return $cipher_bash64;
+    }
+
+    private function decrypt($cipher)
+    {
+        $cipher_bash64 = '';
+        $cipher_bash64 = base64_decode($cipher);
+        $plain_text = openssl_decrypt($cipher_bash64, "DES-EDE3", $this->get_key());
+        return $plain_text;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TextController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/', [TextController::class, 'index']);
+Route::post('/store', [TextController::class, 'store']);
+Route::post('/encrypt', [TextController::class, 'get_encrypyt']);
+Route::post('/decrypt', [TextController::class, 'get_decrypyt']);
